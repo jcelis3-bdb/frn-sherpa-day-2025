58fc38757d2057819032da7c358c145a3a62a7e4
Jhonatan_Celis🐙 <jhonatan.celis@hotmail.com>
2025-09-25 10:38:23 -0500

    Se agrega SHERPA v6.3.9

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { defineCustomElements } from '@npm-bbta/bbog-dig-dt-sherpa-lib/loader';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+defineCustomElements();
